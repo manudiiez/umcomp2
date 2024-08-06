6088bea1736660e1575990b43230279cf3caadc0
Manuel Diez de Oñate <manudiiez@MacBook-Pro-de-Manuel.local>
2024-08-05 21:53:25 -0300

    tp1 terminado

--- tp1/main.py
+++ tp1/main.py
@@ -13,7 +13,7 @@ def cargar_imagen(ruta_imagen):
 
 def dividir_imagen(imagen, n):
     imagen_np = np.array(imagen)  # Convierte la imagen a un array de numpy
-    partes = np.array_split(imagen_np, n, axis=0)  # Divide la imagen en n partes a lo largo del eje vertical
+    partes = np.array_split(imagen_np, n, axis=0) 
     return partes
 
 def aplicar_filtro(parte_imagen, filtro_seleccionado):
@@ -31,66 +31,64 @@ def aplicar_filtro(parte_imagen, filtro_seleccionado):
     if filtro_seleccionado not in filtros:
         raise ValueError(f"Filtro no válido: {filtro_seleccionado}")
 
-    parte_filtrada = filtros[filtro_seleccionado](parte_imagen)  # Aplica el filtro seleccionado a la parte de la imagen
+    parte_filtrada = filtros[filtro_seleccionado](parte_imagen) 
     return parte_filtrada
 
 def procesar_parte(parte, shared_memory, shape, index, conn, filtro_seleccionado):
-    resultado = aplicar_filtro(parte, filtro_seleccionado)  # Aplica el filtro a la parte de la imagen
-    shared_array = np.frombuffer(shared_memory.get_obj()).reshape(shape)  # Obtiene la memoria compartida como un array numpy
-    shared_array[index:index+parte.shape[0], :, :] = resultado  # Guarda la parte filtrada en la memoria compartida
-    conn.send('done')  # Envía una señal de que el proceso ha terminado
-    conn.close()  # Cierra la conexión del pipe
-
+    resultado = aplicar_filtro(parte, filtro_seleccionado)  
+    shared_array = np.frombuffer(shared_memory.get_obj()).reshape(shape) 
+    shared_array[index:index+parte.shape[0], :, :] = resultado 
+    conn.send('done') 
+    conn.close() 
 def proceso_coordinador(pipes, event, n):
     try:
         for i in range(n):
-            pipes[i][0].recv()  # Espera la señal de que cada proceso ha terminado
-            pipes[i][0].close()  # Cierra la conexión del pipe
+            pipes[i][0].recv()  
+            pipes[i][0].close() 
 
-        event.set()  # Señala que el procesamiento ha terminado
+        event.set() 
     except (KeyboardInterrupt, SystemExit):
         print("Proceso coordinador interrumpido. Terminando de manera controlada.")
         event.set()
         sys.exit(0)
 
 def crear_procesos_y_procesar(shared_memory, shape, partes, event, filtro_seleccionado):
-    n = len(partes)  # Número de partes en que se dividió la imagen
-    pipes = [mp.Pipe() for _ in range(n)]  # Crea un pipe para cada proceso secundario
+    n = len(partes) 
+    pipes = [mp.Pipe() for _ in range(n)] 
     procesos = [
         mp.Process(target=procesar_parte, args=(partes[i], shared_memory, shape, sum(part.shape[0] for part in partes[:i]), pipes[i][1], filtro_seleccionado))
         for i in range(n)
-    ]  # Crea un proceso para cada parte de la imagen
-    proceso_coord = mp.Process(target=proceso_coordinador, args=(pipes, event, n))  # Crea el proceso coordinador
+    ]  
+    proceso_coord = mp.Process(target=proceso_coordinador, args=(pipes, event, n))  
     
     for proceso in procesos:
-        proceso.start()  # Inicia cada proceso secundario
+        proceso.start()  
     
-    proceso_coord.start()  # Inicia el proceso coordinador
+    proceso_coord.start()  
     
     try:
         for proceso in procesos:
-            proceso.join()  # Espera a que cada proceso secundario termine
+            proceso.join()  
 
-        proceso_coord.join()  # Espera a que el proceso coordinador termine
+        proceso_coord.join()  
     except (KeyboardInterrupt, SystemExit):
         print("Interrupción recibida. Terminando subprocesos y proceso coordinador de manera controlada.")
         event.set()
         for proceso in procesos:
-            proceso.terminate()  # Termina cada proceso secundario
-        proceso_coord.terminate()  # Termina el proceso coordinador
+            proceso.terminate() 
+        proceso_coord.terminate()
         sys.exit(0)
 
 def guardar_imagen(shared_memory, shape, ruta_salida):
-    imagen_completa = np.frombuffer(shared_memory.get_obj()).reshape(shape)  # Obtiene la imagen completa desde la memoria compartida
-    imagen_final = Image.fromarray(np.uint8(imagen_completa))  # Convierte el array numpy en una imagen PIL
-    imagen_final.save(ruta_salida)  # Guarda la imagen en la ruta especificada
-
+    imagen_completa = np.frombuffer(shared_memory.get_obj()).reshape(shape)  
+    imagen_final = Image.fromarray(np.uint8(imagen_completa)) 
+    imagen_final.save(ruta_salida) 
 def proceso_principal(shared_memory, shape, start_time, image_output, event):
     try:
-        event.wait()  # Espera hasta que el evento indique que el procesamiento ha terminado
-        guardar_imagen(shared_memory, shape, image_output)  # Guarda la imagen final procesada
-        total_time = time.time() - start_time  # Calcula el tiempo total de procesamiento
-        print(f'Tiempo total: {total_time}')  # Imprime el tiempo total
+        event.wait() 
+        guardar_imagen(shared_memory, shape, image_output) 
+        total_time = time.time() - start_time  
+        print(f'Tiempo total: {total_time}') 
     except (KeyboardInterrupt, SystemExit):
         print("Interrupción en el proceso principal. Terminando de manera controlada. funcion proceso_principal")
         sys.exit(0)
@@ -106,9 +104,7 @@ def mostrar_ayuda():
 if __name__ == "__main__":
     # Manejador de señales
     signal.signal(signal.SIGINT, manejador_senal)
-    # signal.signal(signal.SIGTSTP, manejador_senal)
 
-    # Obtener los argumentos de la línea de comandos
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hf:n:", ["filtro=", "partes="])
     except getopt.GetoptError as err:
@@ -167,6 +163,7 @@ if __name__ == "__main__":
     
     # Inician los procesos
     principal.start()
+    # time.sleep(3)
     secundario.start()
 
     try:
