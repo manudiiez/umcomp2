87366ed86bfe9695c4c83ff9a98b3613b6c60602
Manuel Diez de Oñate <manudiiez@MacBook-Pro-de-Manuel.local>
2024-11-08 09:33:35 -0300

    iniciando tp 2

---
+++ tp2/main.py
@@ -0,0 +1,17 @@
+import asyncio
+
+def cargar_imagen(ruta_imagen):
+    print("Cargando imagen...")
+
+
+def aplicar_filtro(parte_imagen, filtro_seleccionado):
+   print("Aplicando filtro...")
+     
+def guardar_imagen(shared_memory, shape, ruta_salida):
+    print("Guardando imagen...")
+   
+def proceso_principal(shared_memory, shape, start_time, image_output, event):
+    print("Proceso principal...")
+
+if __name__ == "__main__":
+    print("Iniciando programa...")
